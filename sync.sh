b5e0f25c53be340185d9053952d8c52ecaf6e524
Pau Vilella <pau.vilella@apple.com>
2025-02-19 12:19:00 +0100

    Add other config files

--- sync.sh
+++ sync.sh
@@ -6,4 +6,20 @@ ln -s $HOME/dotfiles/.config/nvim $HOME/.config/nvim
 
 # zsh
 rm $HOME/.zshrc
-ln -s $HOME/dotfiles/.zshrc $HOME/.zsh
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+
+# GitHub cli
+rm -rf $HOME/.config/gh
+ln -s $HOME/dotfiles/.config/gh $HOME/.config/gh
+
+# Cobra
+rm $HOME/.cobra.yaml
+ln -s $HOME/dotfiles/.cobra.yaml $HOME/.cobra.yaml
+
+# Starship
+rm $HOME/.config/starship.toml
+ln -s $HOME/dotfiles/.config/starship.toml $HOME/.config/starship.toml
+
+# Ghostty
+rm -rf $HOME/.config/ghostty
+ln -s $HOME/dotfiles/.config/ghostty $HOME/.config/ghostty
