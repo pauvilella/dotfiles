42338e7a517b822f41b872784ff0384e66f701e4
Pau Vilella <pau.vilella.st@gmail.com>
2025-03-03 16:36:44 +0100

    Add Brewfile to dotfiles

---
+++ setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# zsh
+echo "Setting up ZSH..."
+rm $HOME/.zshrc
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+
+# SSH
+echo "Setting up SSH..."
+rm $HOME/.ssh/config
+ln -s $HOME/dotfiles/.ssh/config $HOME/.ssh/config
+
+# Git
+echo "Setting up Git..."
+rm $HOME/.gitconfig
+ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+
+# Nvim
+echo "Setting up NeoVim..."
+rm -rf $HOME/.config/nvim
+ln -s $HOME/dotfiles/.config/nvim $HOME/.config/nvim
+
+# Tmux
+echo "Setting up Tmux..."
+rm $HOME/.tmux.conf
+ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+
+# Ghostty
+echo "Setting up Ghostty..."
+rm -rf $HOME/.config/ghostty
+ln -s $HOME/dotfiles/.config/ghostty $HOME/.config/ghostty
+
+# Starship
+echo "Setting up Starship..."
+rm $HOME/.config/starship.toml
+ln -s $HOME/dotfiles/.config/starship.toml $HOME/.config/starship.toml
+
+# GitHub CLI
+echo "Setting up GitHub CLI..."
+rm -rf $HOME/.config/gh
+ln -s $HOME/dotfiles/.config/gh $HOME/.config/gh
+
+# Cobra
+echo "Setting up Cobra..."
+rm $HOME/.cobra.yaml
+ln -s $HOME/dotfiles/.cobra.yaml $HOME/.cobra.yaml
+
+# Brew
+echo "Setting up Brew and installing formulas..."
+if ! command -v brew &>/dev/null; then
+    echo "Homebrew is not installed. Installing now..."
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # Ensure Homebrew is available in the current session
+    eval "$(/opt/homebrew/bin/brew shellenv 2>/dev/null)"
+else
+    echo "Homebrew is already installed!"
+fi
+echo "Updating brew..."
+brew update
+echo "Installing brew taps, formulas and casks..."
+brew bundle --file=$HOME/dotfiles/Brewfile
+echo "Installing Work related brew taps, formulas and casks..."
+brew bundle --file=$HOME/.work/Brewfile
+echo "Upgrading brew"
+
+echo "All done! :)"
---
+++ setup/new-mac.md
@@ -0,0 +1,43 @@
+# Setup a new Mac
+
+## Introduction
+
+This guide will walk through the steps to setup a new Mac for development.
+
+## Initial steps
+
+Set the hostname:
+
+- `sudo scutil --set LocalHostName <my-hostname>`
+- `sudo scutil --set HostName <my-hostname>`
+- `sudo scutil --set ComputerName <my-hostname>`
+
+Set keyboard speed:
+
+- `defaults write -g KeyRepeat -int 1.15`
+- `defaults write -g InitialKeyRepeat -int 20`
+
+Install and update mac software:
+
+- `softwareupdate --install-rosetta`
+- `xcode-select --install`
+
+Oy My Zsh:
+
+- `sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"`
+- `git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions`
+- `git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my- zsh/custom}/plugins/zsh-syntax-highlighting`
+
+## Create a new SSH key and upload it to the Github Profile
+
+1. Create a new SSH key: `ssh-keygen -t ed25519 -C <my-hostname>`
+2. Copy public key to clipboard: `pbcopy < ~/.ssh/id_ed25519.pub`
+3. Add it to the Github Profile.
+4. Createa a GPG key for signing commits. Following GitHub's [instructions](https://docs.github.com/en/authentication/managing-commit-signature-verification/generating-a-new-gpg-key).
+
+## Create Git repo structure
+
+Inside the `git/work` folder I'll add the Work specific orgs, and inside the `git/personal` folder the personal specific orgs.
+
+- `mkdir -p $HOME/git/work`
+- `mkdir -p $HOME/git/personal
