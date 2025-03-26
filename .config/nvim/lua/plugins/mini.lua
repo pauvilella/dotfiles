10798f8ef382be2c20ee45795d395da77a47db52
Pau Vilella <pau.vilella.st@gmail.com>
2025-03-26 20:19:40 +0100

    try to fix terraform support

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -9,5 +9,22 @@ return {
     require('mini.pairs').setup()
     require('mini.tabline').setup()
     require('mini.comment').setup()
+    require('mini.move').setup {
+      mappings = {
+        -- Move visual selection in Visual mode.
+        left = '<S-h>',
+        right = '<S-l>',
+        down = '<S-j>',
+        up = '<S-k>',
+        -- Move current line in Normal mode
+        line_left = '<S-h>',
+        line_right = '<S-l>',
+        line_down = '<S-j>',
+        line_up = '<S-k>',
+      },
+      options = {
+        reindent_linewise = true,
+      },
+    }
   end,
 }
