ccb7b3f69374fa534850970737ddd100e24d47c4
Pau Vilella <pau.vilella.st@gmail.com>
2025-04-01 14:23:14 +0200

    add new actions to copy the filename, seach in directory and perform diff between to files in the snacks.nvim explorer

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -35,6 +35,105 @@ return {
       sources = {
         explorer = {
           hidden = true,
+          actions = {
+            copy_file_path = {
+              action = function(_, item)
+                if not item then
+                  return
+                end
+
+                local vals = {
+                  ["Basename"] = vim.fn.fnamemodify(item.file, ":t:r"),
+                  ["Extension"] = vim.fn.fnamemodify(item.file, ":t:e"),
+                  ["Filename"] = vim.fn.fnamemodify(item.file, ":t"),
+                  ["Path"] = item.file,
+                  ["Path (CWD)"] = vim.fn.fnamemodify(item.file, ":."),
+                  ["Path (HOME)"] = vim.fn.fnamemodify(item.file, ":~"),
+                  ["Uri"] = vim.uri_from_fname(item.file),
+                }
+
+                local options = vim.tbl_filter(function(val)
+                  return vals[val] ~= ""
+                end, vim.tbl_keys(vals))
+                if vim.tbl_isempty(options) then
+                  vim.notify("No values to copy", vim.log.levels.WARN)
+                  return
+                end
+                table.sort(options)
+                vim.ui.select(options, {
+                  prompt = "Choose to copy to clipboard:",
+                  format_item = function(list_item)
+                    return ("%s: %s"):format(list_item, vals[list_item])
+                  end,
+                }, function(choice)
+                  local result = vals[choice]
+                  if result then
+                    vim.fn.setreg("+", result)
+                    Snacks.notify.info("Yanked `" .. result .. "`")
+                  end
+                end)
+              end,
+            },
+            search_in_directory = {
+              action = function(_, item)
+                if not item then
+                  return
+                end
+                local dir = vim.fn.fnamemodify(item.file, ":p:h")
+                Snacks.picker.grep({
+                  cwd = dir,
+                  cmd = "rg",
+                  args = {
+                    "-g",
+                    "!.git",
+                    "-g",
+                    "!node_modules",
+                    "-g",
+                    "!dist",
+                    "-g",
+                    "!build",
+                    "-g",
+                    "!coverage",
+                    "-g",
+                    "!.DS_Store",
+                    "-g",
+                    "!.docusaurus",
+                    "-g",
+                    "!.dart_tool",
+                  },
+                  show_empty = true,
+                  hidden = true,
+                  ignored = true,
+                  follow = false,
+                  supports_live = true,
+                })
+              end,
+            },
+            diff = {
+              action = function(picker)
+                picker:close()
+                local sel = picker:selected()
+                if #sel > 0 and sel then
+                  Snacks.notify.info(sel[1].file)
+                  vim.cmd("tabnew " .. sel[1].file)
+                  vim.cmd("vert diffs " .. sel[2].file)
+                  Snacks.notify.info("Diffing " .. sel[1].file .. " against " .. sel[2].file)
+                  return
+                end
+
+                Snacks.notify.info("Select two entries for the diff")
+              end,
+            },
+          },
+          win = {
+            list = {
+              keys = {
+                ["y"] = "copy_file_path",
+                ["s"] = "search_in_directory",
+                ["D"] = "diff",
+              },
+            },
+          },
         },
       },
     },
