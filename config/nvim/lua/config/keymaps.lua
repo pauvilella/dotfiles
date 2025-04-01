80db52842cf1bcbc5fa30bc3430e821162654055
Pau Vilella <pau.vilella.st@gmail.com>
2025-04-01 14:05:48 +0200

    add new select all keymap

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("n", "<leader>a", ":keepjumps normal! ggVG<cr>", { desc = "Select all" })
+
 vim.keymap.set("n", "<leader>sx", require("snacks.picker").resume, { noremap = true, silent = true, desc = "Resume" })
 
 vim.keymap.set("n", "<leader>k", function()
